2337e46d82a8120136d92346779252aa1d52138c
Nazaruk-D <nazaruk-dima@mail.ru>
2023-04-09 16:09:27 +0300

    first commit

---
+++ usersController.ts
@@ -0,0 +1,13 @@
+import {connection} from "./index";
+
+class usersController {
+    async fetchUsers(req: any, res: any) {
+        try {
+            return res.status(200).send({message: 'Getting users successfully', data: {}, statusCode: 200});
+        } catch (e) {
+            console.log(e)
+        }
+    }
+}
+
+module.exports = new usersController()
---
+++ usersRouter.ts
@@ -0,0 +1,11 @@
+const RouterUsers = require('express')
+const usersController = require('./usersController')
+const usersRouter = new RouterUsers()
+
+const usersEndPoints = {
+    fetchUsers: '/fetch',
+}
+
+usersRouter.get(usersEndPoints.fetchUsers, usersController.fetchUsers)
+
+module.exports = usersRouter
